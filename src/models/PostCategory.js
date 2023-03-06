2795a9c5b765dc1599b327dced650f1e8dd548b7
João <joaolelles7@gmail.com>
2023-03-06 15:55:05 -0300

    Requisito 11, Feat: Criando o modelo PostCategory em src/models/PostCategory.js com as propriedades e associações corretas

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -3,13 +3,27 @@
 const PostCategoryModel = (sequelize, DataTypes) => {
   const postCategory = sequelize.define('PostCategory', {
     postId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER
+    categoryId: DataTypes.INTEGER,
   },
     {
-      tableName: 'blog_posts',
+      tableName: 'posts_categories',
       underscored: true,
       timestamps: false,
     });
+  postCategory.associate = ({ BlogPost, Category }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      through: postCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    Category.belongsToMany(BlogPost, {
+      as: 'blog_posts',
+      through: postCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
   return postCategory;
 };
 
